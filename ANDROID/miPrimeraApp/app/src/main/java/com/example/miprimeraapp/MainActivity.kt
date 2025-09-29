1117459457a735fc7676f50b511b7982f544ae89
Guillermovillacuratorres <127245210+Guillermovillacuratorres@users.noreply.github.com>
2025-09-29 08:32:06 -0300

    Carpeta android

---
+++ ANDROID/miPrimeraApp/app/src/main/java/com/example/miprimeraapp/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.miprimeraapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.miprimeraapp.navigation.AppNavigation
+import com.example.miprimeraapp.ui.theme.MiPrimeraAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MiPrimeraAppTheme {
+                Surface (
+                    modifier = Modifier.fillMaxWidth(),
+                    color = MaterialTheme.colorScheme.background
+                ){
+                    AppNavigation()
+                }
+            }
+        }
+    }
+}
+
---
+++ ANDROID/miPrimeraApp/app/src/main/java/com/example/miprimeraapp/navigation/AppNavigation.kt
@@ -0,0 +1,28 @@
+package com.example.miprimeraapp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+import androidx.navigation.compose.rememberNavController
+import com.example.miprimeraapp.views.login.LoginScreen
+
+
+@Composable
+fun AppNavigation(){
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "login"
+    )
+    {
+        composable("login"){
+            LoginScreen().login()
+        }
+
+
+
+    }
+}
